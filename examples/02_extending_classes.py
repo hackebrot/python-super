10f59f1a72770e4f5b69e74c87af6feb28d4f1a3
Raphael Pierzina <raphael@hackebrot.de>
2019-12-09 17:35:17 +0100

    Add MPL 2.0 comment to code examples

--- examples/02_extending_classes.py
+++ examples/02_extending_classes.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """02 - Extending a class in Python.
 
 Use inheritance to extend the Person class.
